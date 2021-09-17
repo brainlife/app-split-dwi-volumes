ef57408d951ed1f12a79244ba2908b3e7083cc84
Brad Caron <bacaron@iu.edu>
2021-09-17 10:47:41 -0400

    reason: updated for mrtrix3

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dwi=`jq -r '.dwi' config.json`
+bvecs=`jq -r '.bvecs' config.json`
+bvals=`jq -r '.bvals' config.json`
+vols=`jq -r '.vols' config.json`
+
+# make output dirs
+outdir="output"
+[ ! -d ${outdir} ] && mkdir -p ${outdir}
+
+# extract volumes
+[ ! -f ${outdir}/dwi.nii.gz ] && mrconvert -fslgrad ${bvecs} ${bvals} -export_grad_fsl ${outdir}/dwi.bvecs ${outdir}/dwi.bvals -coords 3 ${vols} ${dwi} ${outdir}/dwi.nii.gz -force -nthreads 4
